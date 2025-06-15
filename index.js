492c355afd0f12db6cd8851b3e73c365a6d7df75
Danhtramcam321 <datanh.2706@gmail.com>
2025-06-15 16:31:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,3 +51,9 @@ client.on(Events.InteractionCreate, async interaction => {
 
 // Đăng nhập
 client.login(process.env.TOKEN);
+//Giữ chạy 24/7
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is alive!'));
+app.listen(3000, () => console.log('Web server is running on port 3000'));
